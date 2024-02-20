3cbacadecd45fd865346b71943f21dda94a04aa0
Oscar Fonty <oscar77.fonty@gmail.com>
2024-02-20 16:54:52 +0100

    wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get '/articles', to: 'articles#index'
-  get '/articles/new', to: 'articles#new'
-  get '/articles/:id', to: 'articles#show', as: :article
-  post '/articles', to: 'articles#create'
-  get '/articles/:id/edit', to: 'articles#edit', as: :edit
-  patch '/articles/:id', to: 'articles#update'
-  delete '/articles/:id', to: 'articles#destroy'
+  Rails.application.routes.draw do
+    resources :articles
+  end
 end
